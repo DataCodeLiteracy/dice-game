13e91e7c581e652e12bb7a236d1cba6a09af194c
DataCodeLiteracy <dataliteracy@icloud.com>
2023-10-12 09:05:49 +0900

    feat: gameStart, gameOver 구현 #3
    2023.10.12

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const rollDiceBtn = document.querySelector('.roll-dice')
 const holdBtn = document.querySelector('.hold')
+const newGameBtn = document.querySelector('.new-game')
 const diceImgElem = document.querySelector('.dice-img')
 const diceImgWrap = document.querySelector('.dice-img--wrap')
 const p1CurrentScoreElem = document.querySelector('.player1 .current--score')
@@ -8,6 +9,8 @@ const p1TotalScoreElem = document.querySelector('.player1 .player--total-score')
 const p2TotalScoreElem = document.querySelector('.player2 .player--total-score')
 const p1SectionElem = document.querySelector('.player1')
 const p2SectionElem = document.querySelector('.player2')
+const p1TitleElem = document.querySelector('.player1 .player--title')
+const p2TitleElem = document.querySelector('.player2 .player--title')
 
 let p1CurrentScore = 0
 let p1AccumulateScore = 0
@@ -19,87 +22,123 @@ let p2TotalScore = 0
 
 let playerTurn = true // true : p1, false: p2
 
+const gameOver = () => {
+  p1CurrentScore = 0
+  p1AccumulateScore = 0
+  p1TotalScore = 0
+  p2CurrentScore = 0
+  p2AccumulateScore = 0
+  p2TotalScore = 0
+}
+
+const newGame = () => {
+  newGameBtn.addEventListener('click', () => {
+    p1CurrentScore = 0
+    p1AccumulateScore = 0
+    p1TotalScore = 0
+    p2CurrentScore = 0
+    p2AccumulateScore = 0
+    p2TotalScore = 0
+
+    diceImgElem.classList.remove('active')
+    p1CurrentScoreElem.innerHTML = 0
+    p2CurrentScoreElem.innerHTML = 0
+    p1TotalScoreElem.innerHTML = 0
+    p2TotalScoreElem.innerHTML = 0
+
+    p1TitleElem.style.color = 'black'
+    p2TitleElem.style.color = 'black'
+
+    playerTurn = true
+
+    changeColor()
+  })
+}
+
 const changeColor = () => {
-  if (playerTurn) {
-    p1SectionElem.style.backgroundColor = '#8de89f'
-    p2SectionElem.style.backgroundColor = '#f9f9715d'
+  if (p1TotalScore >= 10 || p2TotalScore >= 10) {
+    if (!playerTurn) {
+      p1SectionElem.style.backgroundColor = 'black'
+      p1TitleElem.style.color = 'white'
+    } else {
+      p2SectionElem.style.backgroundColor = 'black'
+      p2TitleElem.style.color = 'white'
+    }
   } else {
-    p1SectionElem.style.backgroundColor = '#8de89f5a'
-    p2SectionElem.style.backgroundColor = '#F9F871'
+    if (playerTurn) {
+      p1SectionElem.style.backgroundColor = '#8de89f'
+      p2SectionElem.style.backgroundColor = '#f9f9715d'
+    } else {
+      p1SectionElem.style.backgroundColor = '#8de89f5a'
+      p2SectionElem.style.backgroundColor = '#F9F871'
+    }
   }
 }
 
-rollDiceBtn.addEventListener('click', () => {
-  let currentScore = 0
-  currentScore = Math.floor(Math.random() * 6 + 1)
+const gameStart = () => {
+  rollDiceBtn.addEventListener('click', () => {
+    if (p1TotalScore >= 10 || p2TotalScore >= 10) {
+      return
+    }
 
-  diceImgElem.src = `http://127.0.0.1:8080/assets/dice0${currentScore}.png`
+    let currentScore = 0
+    currentScore = Math.floor(Math.random() * 6 + 1)
 
-  diceImgElem.classList.add('active')
+    diceImgElem.src = `http://127.0.0.1:8080/assets/dice0${currentScore}.png`
 
-  if (currentScore < 3) {
+    diceImgElem.classList.add('active')
+
+    if (currentScore < 3) {
+      if (playerTurn) {
+        p1CurrentScore = 0
+        p1CurrentScoreElem.innerHTML = 0
+        p1AccumulateScore = 0
+        playerTurn = !playerTurn
+      } else if (!playerTurn) {
+        p2CurrentScore = 0
+        p2CurrentScoreElem.innerHTML = 0
+        p2AccumulateScore = 0
+        playerTurn = !playerTurn
+      }
+    } else if (currentScore >= 3) {
+      if (playerTurn) {
+        p1CurrentScore = currentScore
+        p1AccumulateScore += p1CurrentScore
+        p1CurrentScoreElem.innerHTML = p1AccumulateScore
+      } else if (!playerTurn) {
+        p2CurrentScore = currentScore
+        p2AccumulateScore += p2CurrentScore
+        p2CurrentScoreElem.innerHTML = p2AccumulateScore
+      }
+    }
+
+    changeColor()
+  })
+
+  holdBtn.addEventListener('click', () => {
+    if (p1TotalScore >= 10 || p2TotalScore >= 10) {
+      return
+    }
+
     if (playerTurn) {
-      p1CurrentScore = 0
+      p1TotalScore = p1TotalScore + p1AccumulateScore
+      p1TotalScoreElem.innerHTML = p1TotalScore
       p1CurrentScoreElem.innerHTML = 0
       p1AccumulateScore = 0
+      p1CurrentScore = 0
       playerTurn = !playerTurn
     } else if (!playerTurn) {
-      p2CurrentScore = 0
+      p2TotalScore = p2TotalScore + p2AccumulateScore
+      p2TotalScoreElem.innerHTML = p2TotalScore
       p2CurrentScoreElem.innerHTML = 0
       p2AccumulateScore = 0
+      p2CurrentScore = 0
       playerTurn = !playerTurn
     }
-  } else if (currentScore >= 3) {
-    if (playerTurn) {
-      p1CurrentScore = currentScore
-      p1AccumulateScore += p1CurrentScore
-      p1CurrentScoreElem.innerHTML = p1AccumulateScore
-    } else if (!playerTurn) {
-      p2CurrentScore = currentScore
-      p2AccumulateScore += p2CurrentScore
-      p2CurrentScoreElem.innerHTML = p2AccumulateScore
-    }
-  }
 
-  changeColor()
+    changeColor()
+  })
+}
 
-  console.log(playerTurn)
-
-  console.log('p1CurrentScore', p1CurrentScore)
-  console.log('p1AccumulateScore', p1AccumulateScore)
-  console.log('p1TotalScore', p1TotalScore)
-
-  console.log('p2CurrentScore', p2CurrentScore)
-  console.log('p2AccumulateScore', p2AccumulateScore)
-  console.log('p2TotalScore', p2TotalScore)
-})
-
-holdBtn.addEventListener('click', () => {
-  if (playerTurn) {
-    p1TotalScore = p1TotalScore + p1AccumulateScore
-    p1TotalScoreElem.innerHTML = p1TotalScore
-    p1CurrentScoreElem.innerHTML = 0
-    p1AccumulateScore = 0
-    p1CurrentScore = 0
-    playerTurn = !playerTurn
-  } else if (!playerTurn) {
-    p2TotalScore = p2TotalScore + p2AccumulateScore
-    p2TotalScoreElem.innerHTML = p2TotalScore
-    p2CurrentScoreElem.innerHTML = 0
-    p2AccumulateScore = 0
-    p2CurrentScore = 0
-    playerTurn = !playerTurn
-  }
-
-  changeColor()
-
-  console.log(playerTurn)
-
-  console.log('p1CurrentScore', p1CurrentScore)
-  console.log('p1AccumulateScore', p1AccumulateScore)
-  console.log('p1TotalScore', p1TotalScore)
-
-  console.log('p2CurrentScore', p2CurrentScore)
-  console.log('p2AccumulateScore', p2AccumulateScore)
-  console.log('p2TotalScore', p2TotalScore)
-})
+newGame()
+gameStart()
